5924c6b7d20ebc6c0723be7dffad93b726c89717
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2024-02-17 10:53:33 +0100

    Add required include file
    (copied from Mozzi, where, IIRC the two of us authored it a year or two ago)

---
+++ IntegerType.h
@@ -0,0 +1,36 @@
+#ifndef INTTYPE_H_
+#define INTTYPE_H_
+
+#include <Arduino.h>
+
+/** @ingroup util
+Provides appropriate integer types that can bit the given number of bytes on this platform (at most 64).
+*/
+template<uint8_t BYTES> struct IntegerType {
+    // at an odd value, such as 3 bytes? Add one more byte (up to at most 8 bytes)..
+    typedef typename IntegerType<(BYTES < 8) ? (BYTES+1) : 8>::unsigned_type unsigned_type;
+    typedef typename IntegerType<(BYTES < 8) ? (BYTES+1) : 8>::signed_type signed_type;
+};
+
+// These are the specializations for the types that we actually assume to exist:
+template<> struct IntegerType<1> {
+    typedef uint8_t unsigned_type;
+    typedef int8_t signed_type;
+};
+
+template<> struct IntegerType<2> {
+    typedef uint16_t unsigned_type;
+    typedef int16_t signed_type;
+};
+
+template<> struct IntegerType<4> {
+    typedef uint32_t unsigned_type;
+    typedef int32_t signed_type;
+};
+
+template<> struct IntegerType<8> {
+    typedef uint64_t unsigned_type;
+    typedef int64_t signed_type;
+};
+
+#endif /* INTTYPE_H_ */
