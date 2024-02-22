5a78b4443c45923f17c6e5a54c1bd8b79a697bc7
tomcombriat <tomcombriat@live.fr>
2024-02-22 21:55:12 +0100

    Updated docs

--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -3,5 +3,5 @@ var searchData=
   ['tosfraction_67',['toSFraction',['../FixMath_8h.html#a46b90b067252e2ceeed03c96ccc55fd4',1,'FixMath.h']]],
   ['tosint_68',['toSInt',['../FixMath_8h.html#a83bd22fe181cddcf4c3faacbe86140fa',1,'FixMath.h']]],
   ['toufraction_69',['toUFraction',['../FixMath_8h.html#aaaaa1dc404829e0ec37bc565d58f394f',1,'FixMath.h']]],
-  ['touint_70',['toUInt',['../FixMath_8h.html#a5160f5ab224e5424f6dd9e608db0dc9d',1,'FixMath.h']]]
+  ['touint_70',['toUInt',['../FixMath_8h.html#aa2666bae0f50341fb0db86af1af47ec9',1,'FixMath.h']]]
 ];
