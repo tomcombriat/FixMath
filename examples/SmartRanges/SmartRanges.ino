6eb81842c85d75a854ea465e19b4ffff46ab4041
tomcombriat <tomcombriat@live.fr>
2024-02-22 19:49:05 +0100

    Added an examples showing Ranges

---
+++ examples/SmartRanges/SmartRanges.ino
@@ -0,0 +1,72 @@
+/*
+ * FixMath Library - Example demonstrating that FixMath internally keeps track of
+ * the range used by a number to avoid promoting too easily
+ *
+ * This example code is in the public domain CONTRARY TO THE LIBRARY itself,
+ * which is licensed the GNU Lesser General Public Licence
+ *
+ */
+
+#include <FixMath.h>
+
+const uint8_t a = 200, b = 150, c = 205, d = 130;
+auto aF = toUInt(a);  // This automatically makes a UFix<8,0> because a is an 8bits type
+auto bF = toUInt(b), cF = toUInt(c), dF = toUInt(d);
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("aF is of type: UFix<");
+  Serial.print(aF.getNI());
+  Serial.print(",");
+  Serial.print(aF.getNF());
+  Serial.println(">");
+
+
+  /*
+Promotion is needed to be sure that the result
+does not overflow. The sum has the potential
+to fill completely a UFix<9,0>
+*/
+  Serial.print("aF+bF is of type: UFix<");
+  Serial.print((aF + bF).getNI());
+  Serial.print(",");
+  Serial.print((aF + bF).getNF());
+  Serial.println(">");
+
+  /*
+Promotion is needed to be sure that the result
+does not overflow. The sum does not fill completely
+a UFix<10,0>. There is still room for a UFix<8,0>
+because 256*3=768 and the maximum a UFix<10,0> can
+hold is 1023.
+*/
+  Serial.print("aF+bF+cF is of type: UFix<");
+  Serial.print((aF + bF + cF).getNI());
+  Serial.print(",");
+  Serial.print((aF + bF + cF).getNF());
+  Serial.println(">");
+
+
+  /*
+As the sum can hold another UFix<8,0>,
+FixMath does not promote here and
+aF+bF+cF has the same type than
+aF+bF+cF+dF.
+*/
+  Serial.print("aF+bF+cF+dF is of type: UFix<");
+  Serial.print((aF + bF + cF + dF).getNI());
+  Serial.print(",");
+  Serial.print((aF + bF + cF + dF).getNF());
+  Serial.println(">");
+
+  Serial.println();
+
+
+
+
+  delay(1000);
+}
